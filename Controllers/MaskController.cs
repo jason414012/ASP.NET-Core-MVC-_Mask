ea45fc2dfc0e23d5d0428132475c4d41fc6733f3
hjhuang <hjhuang@iscom.com.tw>
2022-06-22 09:41:47 +0800

    政府口罩剩餘數量API

---
+++ Controllers/MaskController.cs
@@ -0,0 +1,28 @@
+﻿using Mask.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mask.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MaskController : Controller
+    {
+        public readonly MaskService _maskservice;
+        public MaskController(MaskService maskservice)
+        {
+            _maskservice = maskservice;
+        }
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var maskcount = await _maskservice.GetMaskInfo();
+                return Ok(maskcount);
+            }
+            catch (HttpRequestException ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
---
+++ Service/MaskService.cs
@@ -0,0 +1,24 @@
+﻿using Mask.Models;
+using System.Text.Json;
+
+namespace Mask.Service
+{
+    public class MaskService
+    {
+        public HttpClient Client { get; set; }
+        public MaskService (HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://quality.data.gov.tw/");
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("User-Agent", "QueryMask Sample");
+            Client = client;
+        }
+        public async Task<IEnumerable<MaskInfo>> GetMaskInfo()
+        {
+            var response = await Client.GetAsync("dq_download_json.php?nid=116285&md5_url=2150b333756e64325bdbc4a5fd45fad1");
+            response.EnsureSuccessStatusCode();
+            using var responseStream=await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<IEnumerable<MaskInfo>>(responseStream);
+        }
+    }
+}
